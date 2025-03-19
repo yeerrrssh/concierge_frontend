9d568f3c89e786bef366c2fd30f2cf1e792d4ab9
Danila <64840431+DGDarkKing@users.noreply.github.com>
2025-03-19 12:26:37 +0700

    Fixed CameraFullScreenPage.tsx

--- src/pages/CameraFullScreenPage/components/CameraFullScreenPage.tsx
+++ src/pages/CameraFullScreenPage/components/CameraFullScreenPage.tsx
@@ -1,9 +1,5 @@
 import { Video } from '../../../components/Video';
 
 export const CameraFullScreenPage = () => {
-  return (
-    <div className="relative pt-28 pl-8">
-      <Video hasViolation isOpened />
-    </div>
-  );
+  return <></>;
 };
